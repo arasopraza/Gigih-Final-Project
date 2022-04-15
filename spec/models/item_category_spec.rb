970e8d9dec759286c3e617c9351d19b42392d1bc
arasopraza <aras.opraza@gmail.com>
2022-04-16 02:03:26 +0700

    test(item_category): add spec for validation ItemCategory model

--- spec/models/item_category_spec.rb
+++ spec/models/item_category_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe ItemCategory, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a valid factory" do
+    expect(FactoryBot.build(:item_category)).to be_valid
+  end
+
+  it 'is valid with a item_id and a category_id' do
+    item_category = FactoryBot.build(:item_category)
+    expect(item_category).to be_valid
+  end
+
+  it 'is invalid without a item_id' do
+    item_category = FactoryBot.build(:item_category, item_id: nil)
+    item_category.valid?
+    expect(item_category.errors[:item_id]).to include("can't be blank")
+  end
+
+  it 'is invalid without a category_id' do
+    item_category = FactoryBot.build(:item_category, category_id: nil)
+    item_category.valid?
+    expect(item_category.errors[:category_id]).to include("can't be blank")
+  end
+
+  it 'is invalid with a duplicate item_id and category_id' do
+    item_category1 = FactoryBot.create(:item_category, item_id: 1, category_id: 1)
+    item_category2 = FactoryBot.build(:item_category, item_id: 1, category_id: 1)
+    item_category2.valid?
+    expect(item_category2.errors[:item_id]).to include("has already been taken")
+    expect(item_category2.errors[:category_id]).to include("has already been taken")
+  end
 end

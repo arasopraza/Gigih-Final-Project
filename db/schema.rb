5a90612765d6592b34ded6cdcbb2d684b6cac3dc
arasopraza <aras.opraza@gmail.com>
2022-04-16 01:13:15 +0700

    Generate model for Customer

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_15_172828) do
+ActiveRecord::Schema.define(version: 2022_04_15_181204) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "customers", force: :cascade do |t|
+    t.string "name"
+    t.integer "phone"
+    t.text "address"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
---
+++ spec/factories/customers.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :customer do
+    name { "MyString" }
+    phone { 1 }
+    address { "MyText" }
+  end
+end

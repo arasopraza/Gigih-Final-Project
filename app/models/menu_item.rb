4328a63c7be94e19990390f00c51c9e8846a8891
arasopraza <aras.opraza@gmail.com>
2022-04-15 23:47:09 +0700

    feat: add validation for MenuItem model

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -1,2 +1,5 @@
 class MenuItem < ApplicationRecord
+    validates :name, presence: true, length: { minimum: 2 }, uniqueness: true
+    validates :description, presence: true, length: { maximum: 150 }
+    validates :price, numericality: { greater_than_or_equal_to: 0.01 }
 end

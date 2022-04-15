7efea53da031376c373d9d19cbf218f26b53dfc9
arasopraza <aras.opraza@gmail.com>
2022-04-16 01:35:45 +0700

    test(customer): add spec for validation Customer model

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a valid factory" do
+    expect(FactoryBot.build(:customer)).to be_valid
+  end
+
+  it 'is valid with a name, phone, address' do
+    customer = FactoryBot.build(:customer)
+    expect(customer).to be_valid
+  end
+
+  it 'is invalid without a name' do
+    customer = FactoryBot.build(:customer, name: nil)
+    customer.valid?
+    expect(customer.errors[:name]).to include("can't be blank")
+  end
+
+  it 'is invalid without a phone' do
+    customer = FactoryBot.build(:customer, phone: nil)
+    customer.valid?
+    expect(customer.errors[:phone]).to include("can't be blank")
+  end
 end

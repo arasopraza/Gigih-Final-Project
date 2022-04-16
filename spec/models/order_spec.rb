156c1e4b7f410275221e62f7b65a3a258df99c69
arasopraza <aras.opraza@gmail.com>
2022-04-16 15:27:52 +0700

    test(order): add spec for validation Order model

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'has a valid factory' do
+    expect(FactoryBot.build(:order)).to be_valid
+  end
+
+  it 'is valid with a customer_id, total, order_date' do
+    order = FactoryBot.build(:order)
+    expect(order).to be_valid
+  end
+
+  it 'is invalid without a customer_id' do
+    order = FactoryBot.build(:order, customer_id: nil)
+    order.valid?
+    expect(order.errors[:customer_id]).to include("can't be blank")
+  end
+
+  it 'is invalid without a total' do
+    order = FactoryBot.build(:order, total: nil)
+    order.valid?
+    expect(order.errors[:total]).to include("can't be blank")
+  end
+
+  it 'is invalid without a order_date' do
+    order = FactoryBot.build(:order, order_date: nil)
+    order.valid?
+    expect(order.errors[:order_date]).to include("can't be blank")
+  end
+
+  it 'is invalid with a non numeric values total' do
+    order = FactoryBot.build(:order, total: 'Rp.5000')
+    order.valid?
+    expect(order.errors[:total]).to include("is not a number")
+  end
 end

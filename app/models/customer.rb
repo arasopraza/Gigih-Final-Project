d8d94a78a14245e014bcb3e715daf4840c74ab3f
arasopraza <aras.opraza@gmail.com>
2022-04-16 17:28:21 +0700

    feat: add associations for customer & order

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,4 @@
 class Customer < ApplicationRecord
+    has_many :orders
     validates :name, :phone, presence: true
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,5 @@
 class Order < ApplicationRecord
+    belongs_to :customer, optional: true
     validates :customer_id, :total, :order_date, presence: true
     validates :total, numericality: true
 end

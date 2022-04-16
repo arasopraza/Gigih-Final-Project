3a166f3718f3b46b9e262d03f18836e3b95b361e
arasopraza <aras.opraza@gmail.com>
2022-04-16 21:58:30 +0700

    feat: add validation email in Customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,5 @@
 class Customer < ApplicationRecord
     has_many :orders
-    validates :name, :phone, presence: true
+    validates :name, :phone, :email, presence: true
+    validates :email, format: /\w+@\w+\.{1}[a-zA-Z]{2,}/
 end

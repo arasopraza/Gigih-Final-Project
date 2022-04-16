9211989ee320b11e57e6c3bbe20309047b1222cd
arasopraza <aras.opraza@gmail.com>
2022-04-16 17:39:34 +0700

    BREAKING CHANGE: add column email to model Customer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_16_075929) do
+ActiveRecord::Schema.define(version: 2022_04_16_103359) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2022_04_16_075929) do
     t.text "address"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email"
   end
 
   create_table "item_categories", force: :cascade do |t|

5f38b0dcefdcede6fc41a371acb17f406a9cf16f
arasopraza <aras.opraza@gmail.com>
2022-04-17 21:03:47 +0700

    test(orders_controller): add spec for controller

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -3,5 +3,13 @@ FactoryBot.define do
     customer_id { 1 }
     total { 1.5 }
     order_date { "2022-04-16" }
+    status { "NEW" }
+  end
+
+  factory :invalid_order, parent: :order do
+    customer_id { nil }
+    total { nil }
+    order_date { nil }
+    status { nil }
   end
 end

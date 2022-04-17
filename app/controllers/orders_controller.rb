cd883ebc543cbe14752f138512ca3cf912150284
arasopraza <aras.opraza@gmail.com>
2022-04-18 01:23:58 +0700

    feat: implement orders_controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,47 @@
 class OrdersController < ApplicationController
+  def index
+    @orders = Order.all
+  end
+
+  def show
+    @order = Order.find(params[:id])
+  end
+
+  def new
+    @order = Order.new
+  end
+
+  def edit
+    @order = Order.find(params[:id])
+  end
+
+  def create
+    @order = Order.new(order_params)
+
+    respond_to do |format|
+      if @order.save
+        format.html { redirect_to order_url(@order), notice: "Order was successfully created." }
+        format.json { render :show, status: :created, location: @order }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+          format.json { render json: @order.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+  
+  def update
+    @order = Order.find(params[:id])
+
+    if @order.update(order_params)
+      redirect_to @order
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+  
+  private
+  # Only allow a list of trusted parameters through.
+  def order_params
+    params.require(:order).permit(:customer_id, :total, :order_date, :status)
+  end
 end

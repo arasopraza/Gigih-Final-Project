601b33a10b3244e5196a7b6114b64c05ca807555
arasopraza <aras.opraza@gmail.com>
2022-04-18 15:02:35 +0700

    test(item_categories_controller): add spec GET index

--- spec/controllers/item_categories_spec.rb
+++ spec/controllers/item_categories_spec.rb
@@ -1,4 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe ItemCategoriesController do
+  describe 'GET index' do        
+    it 'populates an array of all item_categories' do
+      item1 = create(:item_category, category_id: 9, item_id: 9)
+      item2 = create(:item_category, category_id: 10, item_id: 10)
+      get :index
+      expect(assigns(:item_categories)).to match_array([item1, item2])
+    end
+    
+    it 'return a 200 OK status' do
+      get :index
+      expect(response).to have_http_status(:ok)
+    end
+  end
 end

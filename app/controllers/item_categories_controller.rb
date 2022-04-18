485fb5e338039ddc91bc2d1edde1b17f96a3b80a
arasopraza <aras.opraza@gmail.com>
2022-04-18 15:04:33 +0700

    feat: implement GET index ItemCategory

--- app/controllers/item_categories_controller.rb
+++ app/controllers/item_categories_controller.rb
@@ -1,2 +1,6 @@
 class ItemCategoriesController < ApplicationController
+  def index
+    @item_categories = ItemCategory.all
+    render json: @item_categories
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   resources :menus
   resources :orders
   resources :categories
+  resources :item_categories
 end

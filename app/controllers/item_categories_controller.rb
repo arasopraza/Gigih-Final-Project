6f0dba208533b3de2f922c40fb9add80e0efac3c
arasopraza <aras.opraza@gmail.com>
2022-04-19 16:19:12 +0700

    feat: implement item_categories controller

--- app/controllers/item_categories_controller.rb
+++ app/controllers/item_categories_controller.rb
@@ -1,6 +1,53 @@
 class ItemCategoriesController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  
   def index
     @item_categories = ItemCategory.all
-    render json: @item_categories
+  end
+
+  def new
+    @item_category = ItemCategory.new
+  end
+
+  def edit
+    @item_category = ItemCategory.find(params[:id])
+  end
+
+  def create
+    @item_category = ItemCategory.new(item_category_params)
+
+    respond_to do |format|
+      if @item_category.save
+        format.html { redirect_to item_category_url(@item_category), notice: "item_category was successfully created." }
+        format.json { render :show, status: :created, location: @item_category }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @item_category.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+  
+  def update
+    @item_category = ItemCategory.find(params[:id])
+
+    if @item_category.update(item_category_params)
+      redirect_to @item_category
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+  
+  def destroy
+    @item_category = ItemCategory.find(params[:id])
+
+    @item_category.destroy
+
+    redirect_to action: "index"
+  end
+
+  private
+  # Only allow a list of trusted parameters through.
+  def item_category_params
+    params.require(:item_category).permit(:category_id, :item_id)
   end
 end

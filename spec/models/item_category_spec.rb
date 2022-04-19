49249f6c425eb8c0298e98e266552fa6648c0312
arasopraza <aras.opraza@gmail.com>
2022-04-19 23:32:03 +0700

    test(item_categories): change column name at spec model

--- spec/models/item_category_spec.rb
+++ spec/models/item_category_spec.rb
@@ -5,15 +5,15 @@ RSpec.describe ItemCategory, type: :model do
     expect(FactoryBot.build(:item_category)).to be_valid
   end
 
-  it 'is valid with a item_id and a category_id' do
+  it 'is valid with a menu_id and a category_id' do
     item_category = FactoryBot.build(:item_category)
     expect(item_category).to be_valid
   end
 
-  it 'is invalid without a item_id' do
-    item_category = FactoryBot.build(:item_category, item_id: nil)
+  it 'is invalid without a menu_id' do
+    item_category = FactoryBot.build(:item_category, menu_id: nil)
     item_category.valid?
-    expect(item_category.errors[:item_id]).to include("can't be blank")
+    expect(item_category.errors[:menu_id]).to include("can't be blank")
   end
 
   it 'is invalid without a category_id' do
@@ -22,11 +22,11 @@ RSpec.describe ItemCategory, type: :model do
     expect(item_category.errors[:category_id]).to include("can't be blank")
   end
 
-  it 'is invalid with a duplicate item_id and category_id' do
-    item_category1 = FactoryBot.create(:item_category, item_id: 1, category_id: 1)
-    item_category2 = FactoryBot.build(:item_category, item_id: 1, category_id: 1)
+  it 'is invalid with a duplicate menu_id and category_id' do
+    item_category1 = FactoryBot.create(:item_category, menu_id: 1, category_id: 1)
+    item_category2 = FactoryBot.build(:item_category, menu_id: 1, category_id: 1)
     item_category2.valid?
-    expect(item_category2.errors[:item_id]).to include("has already been taken")
+    expect(item_category2.errors[:menu_id]).to include("has already been taken")
     expect(item_category2.errors[:category_id]).to include("has already been taken")
   end
 

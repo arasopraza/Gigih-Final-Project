ae16664596dee1cd3e67f8ecd9c23b5874f2a306
arasopraza <aras.opraza@gmail.com>
2022-04-19 23:38:03 +0700

    test(item_categories): delete duplicate validation spec

--- spec/models/item_category_spec.rb
+++ spec/models/item_category_spec.rb
@@ -22,16 +22,8 @@ RSpec.describe ItemCategory, type: :model do
     expect(item_category.errors[:category_id]).to include("can't be blank")
   end
 
-  it 'is invalid with a duplicate menu_id and category_id' do
-    item_category1 = FactoryBot.create(:item_category, menu_id: 1, category_id: 1)
-    item_category2 = FactoryBot.build(:item_category, menu_id: 1, category_id: 1)
-    item_category2.valid?
-    expect(item_category2.errors[:menu_id]).to include("has already been taken")
-    expect(item_category2.errors[:category_id]).to include("has already been taken")
-  end
-
-  it 'should belong_to menu_item' do
-      expect(ItemCategory.reflect_on_association(:menu_item).macro).to eq :belongs_to
+  it 'should belong_to menu' do
+      expect(ItemCategory.reflect_on_association(:menu).macro).to eq :belongs_to
   end
 
   it "should belong_to category" do

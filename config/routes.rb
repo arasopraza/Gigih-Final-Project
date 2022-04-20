0fe056e2bd45ba64d3c8b64b1e7dd3f377043f58
arasopraza <aras.opraza@gmail.com>
2022-04-20 15:36:15 +0700

    style: change root page and add navbar

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root "item_categories#index"
   resources :menus
   resources :orders
   resources :categories

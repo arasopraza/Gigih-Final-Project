961b60fab16d9d21e15dca1613ef69ca2d9c7ee2
arasopraza <aras.opraza@gmail.com>
2022-04-22 02:06:07 +0700

    test(order_detail): add validation spec for model

--- spec/models/order_detail_spec.rb
+++ spec/models/order_detail_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe OrderDetail, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'has a valid factory' do
+    expect(FactoryBot.build(:order_detail)).to be_valid
+  end
+
+  it 'is valid with attributes' do
+    order_detail = FactoryBot.build(:order_detail)
+    expect(order_detail).to be_valid
+  end
+
+  it 'is invalid without a order_id' do
+    order_detail = FactoryBot.build(:order_detail, order_id: nil)
+    order_detail.valid?
+    expect(order_detail.errors[:order_id]).to include("can't be blank")
+  end
+
+  it 'is invalid without a menu_id' do
+    order_detail = FactoryBot.build(:order_detail, menu_id: nil)
+    order_detail.valid?
+    expect(order_detail.errors[:menu_id]).to include("can't be blank")
+  end
 end

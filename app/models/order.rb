d43b9934d50a6ed2b6c84aa7261f86c71798436c
arasopraza <aras.opraza@gmail.com>
2022-04-22 02:13:09 +0700

    feat: add associations order and order detail

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,6 @@
 class Order < ApplicationRecord
     belongs_to :customer, optional: true
+    has_many :order_detail
     validates :customer_id, :total, :order_date, presence: true
     validates :total, numericality: true
 end
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,3 +1,4 @@
 class OrderDetail < ApplicationRecord
+    belongs_to :order, optional: true
     validates :menu_id, :order_id, presence: true
 end

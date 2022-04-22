9359bd83a42acc1c9b2ba979d156f97860d2dd72
arasopraza <aras.opraza@gmail.com>
2022-04-22 23:50:00 +0700

    feat: implement model menu

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,6 +1,6 @@
 class Menu < ApplicationRecord
     has_many :item_categories
     validates :name, presence: true, length: { minimum: 2 }, uniqueness: true
-    validates :description, presence: true, length: { maximum: 150 }
+    validates :description, length: { maximum: 150 }
     validates :price, numericality: { greater_than_or_equal_to: 0.01 }
 end

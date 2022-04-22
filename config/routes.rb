0941cc3be1ecd53faa3d0729b0e7f37d97f7974c
arasopraza <aras.opraza@gmail.com>
2022-04-23 00:01:22 +0700

    BREAKING CHANGE: change root route

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root "item_categories#index"
+  root "menus#index"
   resources :menus
   resources :orders
   resources :categories
